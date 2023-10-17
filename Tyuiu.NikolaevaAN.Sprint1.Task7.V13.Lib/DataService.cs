fa34a698d662d126056d336ebe54e6cb3376c649
Tyuiu.NikolaevaAN@outlook.com <Tyuiu.NikolaevaAN@outlook.com>
2023-10-18 01:05:06 +0500

    Task7 V13

---
+++ Tyuiu.NikolaevaAN.Sprint1.Task7.V13.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.NikolaevaAN.Sprint1.Task7.V13.Lib
+{
+    public class DataService : ISprint1Task7V13
+    {
+        public double Calculate(double x, double y)
+        {
+            double z = (Math.Pow(y, 2) - Math.Pow(Math.Cos(x), 2) + 10) / (Math.Pow(x, 2) - Math.Pow(Math.Sin(y), 2) + 12);
+            return Math.Round(z, 3);
+        }
+    }
+}
---
+++ Tyuiu.NikolaevaAN.Sprint1.Task7.V13.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NikolaevaAN.Sprint1.Task7.V13.Lib;
+
+namespace Tyuiu.NikolaevaAN.Sprint1.Task7.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double y = 0;
+            double res = ds.Calculate(x, y);
+            double wait = 0.75;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
